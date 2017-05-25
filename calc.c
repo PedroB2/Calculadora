bf0fb4d01758d8663ce9e59eee6ae5855d07eee4
Pedro <a17031@bcc.unifal-mg.edu.br>
2017-05-25 19:06:07 -0300

    Adição da função soma

--- calc.c
+++ calc.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int soma () {}
+int soma (int x, int y) {
+	//alteração feita na branch fsoma
+	return x+y;
+}
 int subtrai () {}
 int multiplica () {}
 int divide () {}
 
 
 int main() {
-	printf("Hello git");
+	printf("Hello git! \n\n");
+	printf("Soma: %d\n", soma(2,2));
+
 } 
