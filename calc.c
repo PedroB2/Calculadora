cf99772fa2a36a5f20b249722e82b48ff3e799f5
Pedro <a17031@bcc.unifal-mg.edu.br>
2017-05-25 19:49:51 -0300

    alteração feita na fdiv

--- calc.c
+++ calc.c
@@ -13,7 +13,10 @@ int multiplica (int x, int y) {
 	//Alteração feita na branch fmult
 	return x*y;
 }
-int divide () {}
+int divide (int x, int y) {
+	//Alteração feita na branch fdiv
+	return x/y;
+}
 
 
 int main() {
@@ -21,4 +24,5 @@ int main() {
 	printf("Soma: %d\n", soma(2,2));
 	printf("Subtração: %d\n", subtrai(2,2));
 	printf("Multiplicação: %d\n", multiplica(2,2));
+	printf("Divisão: %d\n", divide(2,2));
 } 
